4b111f0296b12dd5e24f7c7b6ebc42e8d15cb393
robertvoss <vossrobertt@gmail.com>
2022-07-21 13:48:51 -0300

    Update cotacaomoeda.py

--- cotacaomoeda.py
+++ cotacaomoeda.py
@@ -1,6 +1,9 @@
 import requests
 import json
 
-cotacoes = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
+cotacoes = requests.get('https://economia.awesomeapi.com.br/json/all')
 cotacoes_dic = cotacoes.json()
 print(cotacoes_dic)
+print('Dolar: {}'.format(cotacoes_dic['USD']['bid']))
+print('Euro: {}'.format(cotacoes_dic['EUR']['bid']))
+print('Bitcoin: {}'.format(cotacoes_dic['BTC']['bid']))
